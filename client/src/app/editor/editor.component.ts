4baef80e898d176406e1ddd56e143e282d279f02
vibhortaneja <vtaneja127@gmail.com>
2017-10-24 19:27:09 +0530

    Folder opening

--- client/src/app/editor/editor.component.ts
+++ client/src/app/editor/editor.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { AceEditorModule } from 'ng2-ace-editor'; 
+import { AceEditorModule } from 'ng2-ace-editor';
 import { FormsModule } from '@angular/forms';
 import 'brace';
 import 'brace/ext/language_tools';
-import 'brace/mode/html'; 
+import 'brace/mode/html';
 import 'ace-builds/src-min-noconflict/snippets/html';
 
 
@@ -19,77 +19,91 @@ import { GitService } from '../services/git.service'
 export class EditorComponent implements OnInit {
 
   //declaring variables
-  enter:string;
+  enter: string;
   title = 'app';
   lang: any;
+  url: any = "";
 
-  text:string = "Enter your code here";
+  text: string = "Enter your code here";
   githubUser: any;
   selectedValue: any;
   data: any;
-  fileData:any;
+  fileData: any;
   selectedfile: any;
-   output:any;
-  value:{}
+  output: any;
+  value: {}
 
   options: any = {
-  	maxLines: 1000, 
-  	printMargin: false,
-
+    maxLines: 1000,
+    printMargin: false,
   };
-  val:any
+
+  val: any
 
   //creating the instances of services
-  constructor(private editorService: EditorService,private gitService:GitService){}
+  constructor(private editorService: EditorService, private gitService: GitService) {}
 
   //method to execute the code 
-  run(text){
-    this.enter="output"
-    this.value={run:text
-    
-  }
+  run(text) {
+    this.enter = "output"
+    this.value = {
+      run: text
+
+    }
     this.editorService.runCode(this.value).subscribe(data => {
-      this.output=data.result;
+      this.output = data.result;
       console.log(this.output)
-    }, err=>this.output=err)
+    }, err => this.output = err)
   }
 
   //method to clear the terminal
   clear(text) {
-    this.text=null;
+    this.text = null;
   }
 
   //method to get github repositories
   ngOnInit() {
     this.lang = "javascript";
     this.gitService.getRepos()
-    .subscribe(repos => {
-      this.githubUser = repos;
+      .subscribe(repos => {
+        this.githubUser = repos;
 
-    })
+      })
   }
 
- reposearch()
-      {
-        
-        console.log(this.selectedValue)
-        this.gitService.getTree(this.selectedValue)
-        .subscribe(data=>this.data=data)
-      }
+  reposearch() {
+
+    console.log(this.selectedValue)
+    this.gitService.getTree(this.selectedValue)
+      .subscribe(data => this.data = data)
+  }
 
 
-      showFile(reponame, filename) {
-        this.gitService.getFile(reponame, filename)
-        .subscribe(data=>{
+  showFile(reponame, filename) {
+    this.gitService.openFolder(reponame, filename)
+      .subscribe(
+        data => {
+          this.data = data
+          this.url = this.url + filename + "/"
+          console.log(this.url)
+        }
 
-          this.fileData=data;
+        , err => {
+          this.show(reponame, this.url + filename)
+          this.url = "";
+        })
+  }
 
-          this.text=this.fileData._body;
-        console.log(JSON.stringify(this.text))
-      }, err=>console.log(err))
-      }
+  show(reponame, filename) {
+    this.gitService.getFile(reponame, filename)
+      .subscribe(data => {
+        this.fileData = data;
+        this.text = this.fileData._body;
+      })
+  }
 
-      mode(language) {
-this.lang = language;
-      }
+
+  mode(language) {
+    this.lang = language;
+  }
 }
